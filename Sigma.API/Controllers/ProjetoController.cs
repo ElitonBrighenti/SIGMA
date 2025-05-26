612a339df998f0a2dc7678d02bf2b4159dc0480e
Eliton Brighenti <elitonbrighenti90@gmail.com>
2025-05-26 12:20:58 -0300

    first commit

---
+++ Sigma.API/Controllers/ProjetoController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sigma.Application.Dtos;
+using Sigma.Application.Interfaces;
+using Sigma.Domain.Dtos;
+
+namespace Sigma.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjetoController : ControllerBase
+    {
+        private readonly IProjetoService _projetoService;
+
+        public ProjetoController(IProjetoService projetoService)
+        {
+            _projetoService = projetoService;
+        }
+
+        [HttpPost]
+        [Route("inserir")]
+        public async Task<IActionResult> Inserir([FromBody] ProjetoNovoDto model)
+        {
+            return new JsonResult(await _projetoService.Inserir(model));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Buscar() {
+            var projetos = await _projetoService.BuscarTodos();
+            return Ok(projetos);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(long id)
+        {
+            try
+            {
+                await _projetoService.ExcluirProjetoAsync(id);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] ProjetosDto dto)
+        {
+            try
+            {
+                await _projetoService.AtualizarProjetoAsync(id, dto);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+
+    }
+}
---
+++ Sigma.Application/Services/ProjetoService.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Sigma.Application.Dtos;
+using Sigma.Application.Interfaces;
+using Sigma.Domain.Dtos;
+using Sigma.Domain.Entities;
+using Sigma.Domain.Enums;
+using Sigma.Domain.Interfaces.Repositories;
+
+namespace Sigma.Application.Services
+{
+    public class ProjetoService : IProjetoService
+    {
+        private readonly IMapper _mapper;
+        private readonly IProjetoRepository _projetoRepository;
+        
+        public ProjetoService(IMapper mapper, IProjetoRepository projetoRepository)
+        {
+            _mapper = mapper;
+            _projetoRepository = projetoRepository;
+        }
+
+        public async Task<bool> Inserir(ProjetoNovoDto model)
+        {
+            return await _projetoRepository.Inserir(_mapper.Map<Projeto>(model));
+        }
+        public async Task<List<ProjetosDto>> BuscarTodos()
+        {
+            var projetos = await _projetoRepository.BuscarTodos();
+            return _mapper.Map<List<ProjetosDto>>(projetos);
+        }
+        public async Task ExcluirProjetoAsync(long id)
+        {
+            var projeto = await _projetoRepository.ObterPorIdAsync(id);
+            if (projeto == null)
+                throw new Exception("Projeto não encontrado.");
+
+            if (projeto.Status == StatusProjeto.Iniciado ||
+                projeto.Status == StatusProjeto.Planejado ||
+                projeto.Status == StatusProjeto.EmAndamento ||
+                projeto.Status == StatusProjeto.Encerrado)
+            {
+                throw new Exception("Projetos neste status não podem ser excluídos.");
+            }
+
+            await _projetoRepository.ExcluirAsync(projeto);
+        }
+        public async Task AtualizarProjetoAsync(int id, ProjetosDto dto)
+        {
+            var projeto = await _projetoRepository.ObterPorIdAsync(id);
+            if (projeto == null)
+                throw new Exception("Projeto não encontrado");
+
+            projeto.Nome = dto.Nome;
+            projeto.Status = dto.Status;
+
+            await _projetoRepository.AtualizarAsync(projeto);
+        }
+
+    }
+}
